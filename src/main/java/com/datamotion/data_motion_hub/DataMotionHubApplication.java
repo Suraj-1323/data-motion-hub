624ae1acb20ef96150e55467ae12018553cefd5f
Suraj-1323 <srsingh1323@gmail.com>
2025-07-28 09:24:08 +0530

    Data_Motion_Hub Project basic setup for backend

---
+++ src/main/java/com/datamotion/data_motion_hub/DataMotionHubApplication.java
@@ -0,0 +1,13 @@
+package com.datamotion.data_motion_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMotionHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMotionHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datamotion/data_motion_hub/DataMotionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datamotion.data_motion_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMotionHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
